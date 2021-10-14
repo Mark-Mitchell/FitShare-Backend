92ca6c553133915f6f27b68749d0e1beb604b332
Mark Mitchell <marklukemitchell6@gmail.com>
2021-10-14 03:14:47 +0200

    email validation

--- middleware/verifySignUp.js
+++ middleware/verifySignUp.js
@@ -21,6 +21,16 @@ checkAllDataProvided = (req, res, next) => {
       message:
         "Your password is too easy to guess. Please try another password.",
     });
+
+  function validateEmail(email) {
+    const re =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
+  if (!validateEmail(req.body.email))
+    return res.status(400).send({
+      message: "Your email is invalid. Please use a valid email address.",
+    });
   next();
 };
 
